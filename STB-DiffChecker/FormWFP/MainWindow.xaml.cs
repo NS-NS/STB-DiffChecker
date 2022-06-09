f3d9ef6469586ac8f2a702e27b7b513ce52e726c
SakaiYusuke <sakai.yusuke@nikken.jp>
2022-06-09 13:26:17 +0900

    UserControlを作成し、結果を分離

--- STB-DiffChecker/FormWFP/MainWindow.xaml.cs
+++ STB-DiffChecker/FormWFP/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Win32;
-using NPOI.SS.UserModel;//バージョン2.4.1とする(2020.7.28時点で2.5.1はバグあり)
-using NPOI.XSSF.UserModel;
-using STBDiffChecker.AttributeType;
-using STBDiffChecker.Enum;
 using STBDiffChecker.v201.Records;
+//バージョン2.4.1とする(2020.7.28時点で2.5.1はバグあり)
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -66,22 +63,8 @@ namespace STBDiffChecker
                     dgrdImportance.CanUserAddRows = false;
                     dgrdImportance.CanUserSortColumns = false;
 
-                    //コンボボックスの設定
-                    SetFilterComboBox();
-
-                    // チェックボックス文字変更
-                    ChkCmpNothing.Content = Consistency.ElementIncomparable.ToJapanese();
-                    ChkCmpInconsistent.Content = Consistency.Inconsistent.ToJapanese();
-                    ChkCmpAlmostMatch.Content = Consistency.AlmostMatch.ToJapanese();
-                    ChkCmpConsistent.Content = Consistency.Consistent.ToJapanese();
-
-                    ChkImpNotApplicapable.Content = Importance.NotApplicable.ToJapanese();
-                    ChkImpUnnecessary.Content = Importance.Unnecessary.ToJapanese();
-                    ChkImpOptional.Content = Importance.Optional.ToJapanese();
-                    ChkImpRequired.Content = Importance.Required.ToJapanese();
-
                     // 実行前は結果タブを非表示
-                    TabItemResult.Visibility = Visibility.Hidden;
+                    ((TabItem) TabControl.Items[1]).IsEnabled = false;
                 }
 
             };
@@ -167,16 +150,6 @@ namespace STBDiffChecker
                 DirStbB.Text = path;
                 Validate(path);
             }
-        }
-
-        /// <summary>
-        /// 「閉じる」ボタン
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void BtnClose_Click(object sender, RoutedEventArgs e)
-        {
-            Close();
         }
 
         #region 設定関連
@@ -430,7 +403,7 @@ namespace STBDiffChecker
 
             resultFormSetting = SetSetting();
             RunCompare();
-            TabItemResult.IsSelected = true;//タブ表示を変更
+            TabControl.SelectedIndex = 1; //タブ表示を変更
         }
 
         /// <summary>
@@ -471,7 +444,7 @@ namespace STBDiffChecker
             SetDatePath(totalRecord);
 
             //繰返し操作用にタブをフォーマット
-            TabCntrlResult.Items.Clear();
+            TabItemResult.TabCntrlResult.Items.Clear();
 
             // Summaryの出力
             AddSummary(totalRecord.Summary);
@@ -486,35 +459,12 @@ namespace STBDiffChecker
 
             }
 
-            TabItemResult.Visibility = Visibility.Visible;
+            ((TabItem) TabControl.Items[1]).IsEnabled = true;
+            ((TabItem)TabControl.Items[1]).Header = "結果(変換時刻:" + totalRecord.Summary.dateTime.ToString("yyyy/MM/dd HH:mm:ss") + ")";
             //最初のタブを選択
-            TabCntrlResult.SelectedIndex = 0;
+            TabItemResult.TabCntrlResult.SelectedIndex = 0;
         }
 
-
-        /// <summary>
-        /// フィルタ用のコンボボックス設定
-        /// </summary>
-        private void SetFilterComboBox()
-        {
-            var combo = new List<string>()
-            {
-                Record.JapaneseParentElement,
-                Record.JapaneseKey,
-                Record.JapaneseItem,
-                Record.JapaneseA,
-                Record.JapaneseB,
-                Record.JapaneseComment
-            };
-            foreach (var item in combo)
-            {
-                CmbKey1.Items.Add(item);
-                CmbKey2.Items.Add(item);
-            }
-
-            CmbKey1.SelectedIndex = 0;
-            CmbKey2.SelectedIndex = 1;
-        }
 
         /// <summary>
         /// 指定名称タブを追加する
@@ -542,7 +492,7 @@ namespace STBDiffChecker
             tabItem.Content = dataGrid;
 
             //タブコントロールに追加
-            TabCntrlResult.Items.Add(tabItem);
+            TabItemResult.TabCntrlResult.Items.Add(tabItem);
         }
 
         private void AddSummary(Summary summary)
@@ -566,7 +516,7 @@ namespace STBDiffChecker
             tabItem.Content = dataGrid;
 
             //タブコントロールに追加
-            TabCntrlResult.Items.Add(tabItem);
+            TabItemResult.TabCntrlResult.Items.Add(tabItem);
         }
 
         private void dataGrid_AutoGeneratingColumn(object s, DataGridAutoGeneratingColumnEventArgs e)
@@ -610,7 +560,6 @@ namespace STBDiffChecker
         private void SetDatePath(TotalRecord totalRecord)
         {
             resultFormSetting.dateTime = totalRecord.Summary.dateTime;
-            TabItemResult.Header = "結果(変換時刻:" + totalRecord.Summary.dateTime.ToString("yyyy/MM/dd HH:mm:ss") + ")";
         }
 
         #endregion
@@ -717,217 +666,6 @@ namespace STBDiffChecker
                 return false;
             return true;
         }
-        #endregion
-
-
-        #region フィルタ処理
-        private void UpdateFilter(object sender, EventArgs e)
-        {
-            if (TabCntrlResult?.SelectedItem is TabItem item)
-            {
-                if (item.Content is DataGrid dataGrid)
-                {
-                    if (dataGrid.Name == Summary.DataGridName)
-                        return;
-                    BindingSource sorce = dataGrid.DataContext as BindingSource;
-                    sorce.Filter = MakeFilterText();
-                }
-
-            }
-        }
-
-        private string MakeFilterText()
-        {
-            string filter = string.Empty;
-            List<string> ResultFilter = new List<string>();
-            if (ChkCmpNothing.IsChecked != null && (bool)ChkCmpNothing.IsChecked)
-                ResultFilter.Add(Record.JapaneseConsistency + @" = '" + Consistency.ElementIncomparable.ToJapanese() + "'");
-
-            if (ChkCmpInconsistent.IsChecked != null && (bool)ChkCmpInconsistent.IsChecked)
-                ResultFilter.Add(Record.JapaneseConsistency + @" = '" + Consistency.Inconsistent.ToJapanese() + "'");
-
-            if (ChkCmpAlmostMatch.IsChecked != null && (bool)ChkCmpAlmostMatch.IsChecked)
-                ResultFilter.Add(Record.JapaneseConsistency + @" = '" + Consistency.Incomparable.ToJapanese() + "'");
-
-            //Consistent
-            if (ChkCmpConsistent.IsChecked != null && (bool)ChkCmpConsistent.IsChecked)
-                ResultFilter.Add(Record.JapaneseConsistency + @" = '" + Consistency.Consistent.ToJapanese() + "'");
-
-            if (ResultFilter.Count != 0)
-            {
-                filter = "(" + string.Join(" OR ", ResultFilter) + ")";
-            }
-
-
-            List<string> ImportanceFilter = new List<string>();
-            if (ChkImpNotApplicapable.IsChecked != null && (bool)ChkImpNotApplicapable.IsChecked)
-                ImportanceFilter.Add(Record.JapaneseImportance + @" = '" + Importance.NotApplicable.ToJapanese() + "'");
-
-            if (ChkImpUnnecessary.IsChecked != null && (bool)ChkImpUnnecessary.IsChecked)
-                ImportanceFilter.Add(Record.JapaneseImportance + @" = '" + Importance.Unnecessary.ToJapanese() + "'");
-
-            if (ChkImpOptional.IsChecked != null && (bool)ChkImpOptional.IsChecked)
-                ImportanceFilter.Add(Record.JapaneseImportance + @" = '" + Importance.Optional.ToJapanese() + "'");
-
-            if (ChkImpRequired.IsChecked != null && (bool)ChkImpRequired.IsChecked)
-                ImportanceFilter.Add(Record.JapaneseImportance + @" = '" + Importance.Required.ToJapanese() + "'");
-
-            if (ImportanceFilter.Count != 0)
-            {
-                if (filter != string.Empty)
-                {
-                    filter += " AND ";
-                }
-
-                filter += "(" + string.Join(" OR ", ImportanceFilter) + ")";
-            }
-
-
-            if (!string.IsNullOrEmpty(CmbKey1.SelectedItem?.ToString()) && TxtKey1.Text != string.Empty)
-            {
-                if (filter != string.Empty)
-                {
-                    filter += " AND ";
-                }
-
-                filter += CmbKey1.SelectedItem.ToString() + " LIKE '%" + TxtKey1.Text + "%'";
-            }
-
-            if (!string.IsNullOrEmpty(CmbKey2.SelectedItem?.ToString()) && TxtKey2.Text != string.Empty)
-            {
-                if (filter != string.Empty)
-                {
-                    filter += " AND ";
-                }
-
-                filter += CmbKey2.SelectedItem.ToString() + " LIKE '%" + TxtKey2.Text + "%'";
-            }
-
-            return filter;
-        }
-
-        #endregion
-
-
-        #region Excel処理関連
-        /// <summary>
-        /// 「Excel出力」ボタン
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void BtnExcel_Click(object sender, RoutedEventArgs e)
-        {
-            string path = GetSavePathWithDialog("xlsx ファイル(.xlsx)|*.xlsx|All Files (*.*)|*.*");
-            if (String.IsNullOrWhiteSpace(path) == false)
-            {
-                //以下Excel処理
-                XSSFWorkbook wb;
-                try
-                {
-                    if (File.Exists(path))
-                    {
-                        File.Delete(path);
-                    }
-
-                    wb = new XSSFWorkbook();
-                    wb.CreateSheet("dummy");
-
-                }
-                catch
-                {
-                    System.Windows.Forms.MessageBox.Show("ファイル作成に失敗しました。");
-                    return;
-                }
-
-                ReadDataGrid(wb);
-
-                //1番目のシートは削除
-                wb.RemoveSheetAt(0);
-                wb.SetForceFormulaRecalculation(true);//計算の再実行(これを入れないと計算結果の値が変わらない)
-
-                try
-                {
-                    using (FileStream fs = new FileStream(path, FileMode.Create))
-                    {
-                        wb.Write(fs);
-                    }
-                    System.Windows.Forms.MessageBox.Show("Excelファイルの作成完了。");
-                }
-                catch
-                {
-                    System.Windows.Forms.MessageBox.Show("Excelファイル保存時にエラー発生。");
-                }
-            }
-        }
-
-        /// <summary>
-        /// 結果の読込み
-        /// </summary>
-        private void ReadDataGrid(XSSFWorkbook wb)
-        {
-            foreach (var item in TabCntrlResult.Items)
-            {
-                TabItem tabItem = item as TabItem;
-                DataGrid dataGrid = tabItem.Content as DataGrid;
-                BindingSource source = dataGrid.DataContext as BindingSource;
-                source.Filter = "";
-                DataTable table = ((DataView)source.List).ToTable();
-                WriteExcelSheet(wb, tabItem.Header.ToString(), table);
-            }
-
-        }
-
-        /// <summary>
-        /// Excelシートの作成
-        /// </summary>
-        private void WriteExcelSheet(XSSFWorkbook wb, string SheetName, DataTable table)
-        {
-            //シートの作成
-            ISheet ws = wb.CreateSheet(SheetName);
-
-            //ヘッダの生成
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                writeCellValue(ws, i, 0, table.Columns[i].ColumnName);
-            }
-
-            for (int i = 0; i < table.Columns.Count; i++)
-            {
-                for (int j = 0; j < table.Rows.Count; j++)
-                {
-                    writeCellValue(ws, i, j+1, table.Rows[j][i]);
-                }
-            }
-        }
-
-        // セルへの書込み処理　参照元:https://www.sejuku.net/blog/100771
-        /// <summary>
-        /// セル書き込み(書き込む値が文字列の場合)
-        /// </summary>
-        /// <param name="sheet"></param>
-        /// <param name="idxColumn"></param>
-        /// <param name="idxRow"></param>
-        /// <param name="value"></param>
-        static void writeCellValue(ISheet sheet, int idxColumn, int idxRow, object obj)
-        {
-            var row = sheet.GetRow(idxRow) ?? sheet.CreateRow(idxRow); //指定した行を取得できない時はエラーとならないよう新規作成している
-            var cell = row.GetCell(idxColumn) ?? row.CreateCell(idxColumn); //一行上の処理の列版
-            if (obj == null)
-                cell.SetCellValue(string.Empty);
-
-            string value = obj.ToString();
-            /*
-            double d;
-            if (double.TryParse(value, out d))
-            {
-                cell.SetCellValue(d);
-                cell.SetCellType(CellType.Numeric); 
-            }
-            */
-
-            cell.SetCellValue(value);
-        }
-
         #endregion
 
     }
